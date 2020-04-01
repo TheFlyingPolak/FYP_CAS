f0d8b41fa5ff7032fe6700af2364c2b62a2588fc
TheFlyingPolak <jakubgajewski14@gmail.com>
2020-04-01 22:45:46 +0100

    Fixed agent commands from front end

--- src/main/java/com/gajewski/admin/Controller.java
+++ src/main/java/com/gajewski/admin/Controller.java
@@ -15,9 +15,7 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 public class Controller {
